d8d980114e30fc96d891f05b07fc433770b06bac
Airam Navas <navas.airam@gmail.com>
2016-03-08 13:56:07 +0000

    Main resumido.

--- main.cpp
+++ main.cpp
@@ -15,20 +15,19 @@ int main()
 
     string aux =(string)(expresion);
     size_t found = aux.find("i");
-    if (found!=std::string::npos){
 
-        AEDA::calculadora<complex<float>> A;
-        complex<float> B = A.resolver(expresion);
+    AEDA::calculadora<complex<float>> A;
+    complex<float> B = A.resolver(expresion);
+
+
+    if (found!=std::string::npos){
         if (B.imag() > 0)
             cout << "Resultado: " << B.real() << "+" << B.imag() << "i" << endl;
         else
             cout << "Resultado: " << B.real() << B.imag() << "i" << endl;
     }
-    else{
-        AEDA::calculadora<complex<float>> C;
-        complex<float> D = C.resolver(expresion);
-        cout << "Resultado: " << D.real() << endl;
-    }
+    else
+        cout << "Resultado: " << B.real() << endl;
 
 
     return 0;
